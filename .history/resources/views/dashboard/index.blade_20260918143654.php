4b54847097151a897fa15f9fec6b4a5aa2b3f85d
SayedAbdo2 <sayed220@gmail.com>
2026-09-18 21:40:04 +0300

    ready

---
+++ .history/resources/views/dashboard/index.blade_20260918143654.php
@@ -0,0 +1,13 @@
+@extends('dashboard')
+@section('title', 'starter')
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Main content -->
+    <div class="content">
+        content
+    </div>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+@stop
---
+++ .history/routes/web_20260915204635.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use Inertia\Inertia;
+
+// Route::get('/', function () {
+//     return Inertia::render('welcome');
+// })->name('home');
+
+// Route::middleware(['auth'])->group(function () {
+//     Route::get('dashboard', function () {
+//         return Inertia::render('dashboard');
+//     })->name('dashboard');
+// });
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
