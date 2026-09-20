d2b4071b98481132d3c21e3f7ffa3097b5a8105a
SayedAbdo2 <sayed220@gmail.com>
2026-09-20 14:20:51 +0300

    dbConnection-migration

--- .history/database/migrations/0001_01_01_000000_create_users_table_20260920123219.php
+++ .history/database/migrations/0001_01_01_000000_create_users_table_20260920123219.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('phone_number')->nullable()->unique();
+            $table->string('phone_');
             $table->rememberToken();
             $table->timestamps();
         });
